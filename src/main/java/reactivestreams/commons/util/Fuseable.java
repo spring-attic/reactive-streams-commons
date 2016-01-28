8b46813b96b4116501b16d42eabb18d0bddd9810
akarnokd <akarnokd@gmail.com>
2016-01-28 14:00:30 +0100

    ConditionalSubscriber

--- src/main/java/reactivestreams/commons/util/Fuseable.java
+++ src/main/java/reactivestreams/commons/util/Fuseable.java
@@ -1,8 +1,25 @@
 package reactivestreams.commons.util;
 
+import org.reactivestreams.Subscriber;
+
 /**
  * Marker interface indicating the publisher produces a FusionSubscription.
  */
 public interface Fuseable {
-    
+
+    /**
+     * A subscriber variant that can immediately tell if it consumed
+     * the value or not, avoiding the usual request(1) for dropped
+     * values.
+     *
+     * @param <T> the value type
+     */
+    public interface ConditionalSubscriber<T> extends Subscriber<T> {
+        /**
+         * Try consuming the value and return true if successful.
+         * @param value the value to consume
+         * @return true if consumed, false if dropped and a new value can be immediately sent
+         */
+        boolean tryOnNext(T value);
+    }
 }
