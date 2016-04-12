50ca3e18ab227f8302d191cc6e37f7eee6a9b695
akarnokd <akarnokd@gmail.com>
2016-04-12 16:30:47 +0200

    Remove a looped test

--- src/test/java/reactivestreams/commons/publisher/PublisherCombineLatestTest.java
+++ src/test/java/reactivestreams/commons/publisher/PublisherCombineLatestTest.java
@@ -329,7 +329,7 @@ public class PublisherCombineLatestTest {
         }
     }
 
-        @Test
+//        @Test
     public void asyncInterleavedRacingLoop() {
         for (int i = 0; i < 1000; i++) {
             if (i % 10 == 0) {
