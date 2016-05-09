eb7c197bcf8291206f39d176d81ce23cb9be3239
akarnokd <akarnokd@gmail.com>
2016-05-09 10:16:24 +0200

    Immediate scheduler which can turn observeOn into request-rebatching op

---
+++ src/main/java/rsc/scheduler/ImmediateScheduler.java
@@ -0,0 +1,65 @@
+package rsc.scheduler;
+
+import rsc.flow.Cancellation;
+import rsc.util.*;
+
+/**
+ * Executes tasks on the caller's thread immediately.
+ * <p>
+ * Use the ImmediateScheduler.instance() to get a shared, stateless instance of this scheduler.
+ */
+public final class ImmediateScheduler implements Scheduler {
+
+    private static final ImmediateScheduler INSTANCE = new ImmediateScheduler();
+    
+    public static Scheduler instance() {
+        return INSTANCE;
+    }
+    
+    private ImmediateScheduler() {
+        
+    }
+    
+    static final Cancellation EMPTY = () -> { };
+    
+    @Override
+    public Cancellation schedule(Runnable task) {
+        try {
+            task.run();
+        } catch (Throwable ex) {
+            ExceptionHelper.throwIfFatal(ex);
+            UnsignalledExceptions.onErrorDropped(ex);
+        }
+        return EMPTY;
+    }
+
+    @Override
+    public Worker createWorker() {
+        return new ImmediateSchedulerWorker();
+    }
+    
+    static final class ImmediateSchedulerWorker implements Scheduler.Worker {
+        
+        volatile boolean shutdown;
+
+        @Override
+        public Cancellation schedule(Runnable task) {
+            if (shutdown) {
+                return REJECTED;
+            }
+            try {
+                task.run();
+            } catch (Throwable ex) {
+                ExceptionHelper.throwIfFatal(ex);
+                UnsignalledExceptions.onErrorDropped(ex);
+            }
+            return EMPTY;
+        }
+
+        @Override
+        public void shutdown() {
+            shutdown = true;
+        }
+    }
+
+}
